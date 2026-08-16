211aeab729657726694c3641ea1f9080809dec79
Ali7488 <mohannad.eali@gmail.com>
2026-08-16 10:41:48 -0600

    finished up the logical parts of app, created cart state and added outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router";
 import { useShopApi } from "./hooks/useShopApi";
 import "./styles/App.css";
 
 function App() {
+  // Retrieves products from fakeShop API
   const { error, loading, products } = useShopApi();
+
+  // Cart state, uses ID to retrieve product information from the products array
+  const [cart, setCart] = useState([]);
+
+  function handleAddToCart(itemId, itemQuantity) {
+    setCart((prev) =>
+      prev.some((item) => item.id === itemId)
+        ? prev.map((item) => {
+            if (item.id === itemId) {
+              return { ...item, quantity: itemQuantity };
+            } else return item;
+          })
+        : [...prev, { id: itemId, quantity: itemQuantity }],
+    );
+  }
 
   return (
     <>
@@ -14,7 +31,7 @@ function App() {
         <Link to="/cart">Cart</Link>
       </nav>
       <main>
-        <Outlet context={{ products, loading, error }} />
+        <Outlet context={{ products, loading, error, cart, handleAddToCart }} />
       </main>
       <footer></footer>
     </>
