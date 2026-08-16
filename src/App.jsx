a3df4b8f133d2a6e3fdb3ea76beea09d1390a602
Mohannad Ali <mohannad.eali@gmail.com>
2026-08-15 19:21:36 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <main>
+      {/*
+        1. Change the document title in index.html.
+        2. Change the package name in package.json.
+      */}
+    </main>
+  );
+}
+
+export default App;
