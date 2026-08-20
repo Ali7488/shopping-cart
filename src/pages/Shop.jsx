3a681d560be677e114c96fb40665b0f12e9c6801
Ali7488 <mohannad.eali@gmail.com>
2026-08-19 18:51:27 -0600

    Added Rendering of product cards, plus loading and error handling for the API calls

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,27 @@
+import { useOutletContext } from "react-router";
+import Card from "../components/Card";
+
 export default function Shop() {
+  const { products, handleAddToCart, loading, error } = useOutletContext();
+
+  if (loading) return <h3>Loading our AMAZING products...</h3>;
+  if (error) return <h3>Server Error</h3>;
   return (
     <>
-      <h1>Our Products</h1>
+      <h1>Our Products:</h1>
+
+      {products.map((product) => {
+        return (
+          <Card
+            key={product.id}
+            image={product.image}
+            rating={product.rating}
+            title={product.title}
+            id={product.id}
+            handleAddToCart={handleAddToCart}
+          />
+        );
+      })}
     </>
   );
 }
